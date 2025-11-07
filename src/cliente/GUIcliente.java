4312b16eded3da351213212f7d769e45f28947f2
Michelle-Bell3 <abril.jimenez.71@gmail.com>
2025-11-06 18:56:01 -0600

    u
    u

--- src/cliente/GUIcliente.java
+++ src/cliente/GUIcliente.java
@@ -2,6 +2,6 @@ package cliente;
 
 public class GUIcliente {
     public GUIcliente() {
-        System.out.println("✅ GUIcliente - Asignado a: Abril JBFAWIOFA");
+        System.out.println("✅ GUIcliente - Asignado a: Abril JBFAWIOFA AAAAAAAAAAAAAAAAAAAAAA AAAAAA");
     }
 }
